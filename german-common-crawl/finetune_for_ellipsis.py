80b9f9bed694424bbe5a9b2bd02810c74f466eb0
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-02-24 16:10:18 +0100

    trying something

--- german-common-crawl/finetune_for_ellipsis.py
+++ german-common-crawl/finetune_for_ellipsis.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     test_data = "tüba_test.jsonl"
     train_dataset = load_dataset("json", data_files=train_data, split='train')
     print("Got train data")
-    test_dataset = load_dataset("json", data_files=test_data, split='train')
+    test_dataset = load_dataset("json", data_files=train_data, split='train')
     print("Got test data")
     de_de_dataset = DatasetDict({"train": train_dataset,
                                  "test": test_dataset
