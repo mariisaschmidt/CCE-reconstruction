82bc8c8dbc24d253e79f2920647cfc476aecf3e8
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-03-02 18:22:54 +0100

    more epochs for tiger

--- german-common-crawl/finetune_for_ellipsis_tiger.py
+++ german-common-crawl/finetune_for_ellipsis_tiger.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     per_device_eval_batch_size=4,
     weight_decay=0.01,
     save_total_limit=3,
-    num_train_epochs=5,
+    num_train_epochs=10,
     predict_with_generate=True,
     fp16=True, # set true when cuda available
     push_to_hub=False,
