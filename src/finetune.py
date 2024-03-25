10e7a55d7d83e1e03365f463bec949228413c546
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-03-25 08:51:21 +0100

    using correct number of epochs now

--- src/finetune.py
+++ src/finetune.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
         per_device_eval_batch_size=batchsize,
         weight_decay=0.01,
         save_total_limit=3,
-        num_train_epochs=2,
+        num_train_epochs=epochs,
         predict_with_generate=True,
         fp16=True, # set true when cuda available
         push_to_hub=False,
