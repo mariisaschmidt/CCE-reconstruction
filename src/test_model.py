1eda16d31822c90095b2edf5c919a1576cd964c0
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-03-27 17:49:08 +0100

    more evaluation sentences

--- src/test_model.py
+++ src/test_model.py
@@ -21,11 +21,16 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
-    evaluation_sentences = ["Hallo, wie geht es dir?", 
-                            "Die vorhandene Handlungsbereitschaft wurde in Entscheidungen und Arbeitspakete umgesetzt", 
+    evaluation_sentences = ["Anita kennt jede Zeile , jedes Wort .", # Anita kennt jede Zeile , Anita_f kennt_f jedes Wort . 
+                            "Er war etwa 23 Jahre alt und sah wie ein frecher Junge aus .", # Er war etwa 23 Jahre alt und er_f sah wie ein frecher Junge aus . 
+                            "Ich glaube , es wird genausoviel Trennung von Parlament und Bürger geben .", #;Ich glaube genausoviel_bcr Trennung_bcr von_bcr Parlament_bcr und_bcr Bürger_bcr geben_g , es wird genausoviel Trennung von Parlament und Bürger geben .
                             "Dabei vermengen sich die Konfliktlagen und die Interessen der Akteure",
                             "In den Wahlkampf wird sein Kandidat nicht eingreifen , er bleibt bis September zu Hause .",
-                            "In der Theorie schafft es der Wettbewerb , durch den Konkurrenzgedanken das Streben nach Monopolen und Macht systematisch auszuschalten ."]
+                            "In der Theorie schafft es der Wettbewerb , durch den Konkurrenzgedanken das Streben nach Monopolen und Macht systematisch auszuschalten .",
+                            "Da die Kapazität des Stalles nur für 50 Pferde ausreicht , bleiben die Tiere mitunter auf dem Lkw und kommen am folgenden Tag unversorgt auf den nächsten Transporter oder - jetzt fast immer - auf den Zug . ", #Da die Kapazität des Stalles nur für 50 Pferde ausreicht , bleiben die Tiere mitunter auf dem Lkw und die_s Tiere_s kommen am folgenden Tag unversorgt auf den nächsten Transporter oder - jetzt fast immer - auf den Zug . 
+                            "Am Freitag wurde in rund 15 Universitäten und Hochschulen protestiert , erstmals auch an Fakultäten in Paris . ", #Am Freitag wurde in rund 15 Universitäten und Hochschulen protestiert , erstmals wurde_g am_g Freitag_g auch an Fakultäten in Paris protestiert_g . 
+                            #"",
+    ]
     evaluation_sentences.append(sentence)
     
     predictions = []
@@ -34,11 +39,9 @@ if __name__ == '__main__':
         evaluation_output = model.generate(evaluation_input, max_new_tokens=200)
         decoded = tokenizer.decode(evaluation_output[0])
         decoded = decoded[6:len(decoded)-4]
-        print(s + " -> " + decoded)
+        print(s + "\n -> " + decoded)
         predictions.append(decoded)
         
     metric = evaluate.load("bleu")
     results = metric.compute(predictions=predictions, references=evaluation_sentences)
     print(results)
-
-    # create test dataset and run model on this 
