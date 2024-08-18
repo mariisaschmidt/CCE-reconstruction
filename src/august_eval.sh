c18efa4deb585b5ae1cae2af7d3433146621d956
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-08-18 21:08:14 +0200

    added eval script for overnight evaluation

---
+++ src/august_eval.sh
@@ -0,0 +1,13 @@
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase --corpus tiger --prefix aug18_TuBas
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus tuba --prefix aug18_TiBas
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus tiger --prefix aug18_TiBas
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus eval --prefix aug18_TiBas
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus tuba --prefix aug18_TuSm
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus tiger --prefix aug18_TuSm
+
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus eval --prefix aug18_TuSm
