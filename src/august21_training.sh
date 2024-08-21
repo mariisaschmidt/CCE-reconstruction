38c2909687024bec6d54b4ef0d324fdd26e042dc
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-08-21 13:32:12 +0200

    CCE-01: updated scripts to train new models with updated data files

--- src/august21_training.sh
+++ src/august21_training.sh
@@ -1,18 +1,18 @@
 echo "Start training of TigerSmall"
 
-python3 finetune.py --dataset tiger --model_name cleanTigerSmall --pretrained_model /home/marisa/models/de_de_feb05/checkpoint-929000
+python3 finetune.py --dataset tiger --model_name cleanTigerSmall21 --pretrained_model /home/marisa/models/de_de_feb05/checkpoint-929000
 
 echo "Start training of TübaSmall"
 
-python3 finetune.py --dataset tüba --model_name cleanTübaSmall --pretrained_model /home/marisa/models/de_de_feb05/checkpoint-929000
+python3 finetune.py --dataset tüba --model_name cleanTübaSmall21 --pretrained_model /home/marisa/models/de_de_feb05/checkpoint-929000
 
 echo "Start training of TigerBase"
 
-python3 finetune.py --dataset tiger --model_name cleanTigerBase --pretrained_model /home/marisa/models/de_de_mar14
+python3 finetune.py --dataset tiger --model_name cleanTigerBase21 --pretrained_model /home/marisa/models/de_de_mar14
 
 echo "Start training of TübaBase"
 
-python3 finetune.py --dataset tüba --model_name cleanTübaBase --pretrained_model /home/marisa/models/de_de_mar14
+python3 finetune.py --dataset tüba --model_name cleanTübaBase21 --pretrained_model /home/marisa/models/de_de_mar14
 
 echo "Done with training!"
 echo "Running evaluation script:"
--- src/august_eval.sh
+++ src/august_eval.sh
@@ -1,27 +1,27 @@
 echo "Starting evaluation!"
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall --corpus eval --prefix aug21_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall21 --corpus eval --prefix aug21_TiSmNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall --corpus tuba --prefix aug21_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall21 --corpus tuba --prefix aug21_TiSmNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall --corpus tiger --prefix aug21_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall21 --corpus tiger --prefix aug21_TiSmNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase --corpus eval --prefix aug21_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase21 --corpus eval --prefix aug21_TuBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase --corpus tuba --prefix aug21_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase21 --corpus tuba --prefix aug21_TuBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase --corpus tiger --prefix aug21_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase21 --corpus tiger --prefix aug21_TuBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus tuba --prefix aug21_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase21 --corpus tuba --prefix aug21_TiBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus tiger --prefix aug21_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase21 --corpus tiger --prefix aug21_TiBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase --corpus eval --prefix aug21_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase21 --corpus eval --prefix aug21_TiBasNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus tuba --prefix aug21_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall21 --corpus tuba --prefix aug21_TuSmNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus tiger --prefix aug21_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall21 --corpus tiger --prefix aug21_TuSmNEW
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall --corpus eval --prefix aug21_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall21 --corpus eval --prefix aug21_TuSmNEW
 
 echo "Done with evaluation!"
