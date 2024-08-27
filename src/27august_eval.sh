df992d2e0a53c8d01f641ffadc8416a5c2eef3ac
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-08-27 16:55:17 +0200

    CCE-01: fixed model names

--- src/27august_eval.sh
+++ src/27august_eval.sh
@@ -1,27 +1,27 @@
 echo "Starting evaluation!"
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall27  --corpus eval --prefix 27aug_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerSmall  --corpus eval --prefix 27aug_TiSm
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall27  --corpus tuba --prefix 27aug_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerSmall  --corpus tuba --prefix 27aug_TiSm
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerSmall27  --corpus tiger --prefix 27aug_TiSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerSmall  --corpus tiger --prefix 27aug_TiSm
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase27  --corpus eval --prefix 27aug_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaBase  --corpus eval --prefix 27aug_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase27  --corpus tuba --prefix 27aug_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaBase  --corpus tuba --prefix 27aug_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaBase27  --corpus tiger --prefix 27aug_TuBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaBase  --corpus tiger --prefix 27aug_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase27  --corpus tuba --prefix 27aug_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerBase  --corpus tuba --prefix 27aug_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase27  --corpus tiger --prefix 27aug_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerBase  --corpus tiger --prefix 27aug_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTigerBase27  --corpus eval --prefix 27aug_TiBas
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTigerBase  --corpus eval --prefix 27aug_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall27  --corpus tuba --prefix 27aug_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaSmall  --corpus tuba --prefix 27aug_TuSm
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall27  --corpus tiger --prefix 27aug_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaSmall  --corpus tiger --prefix 27aug_TuSm
 
-python3 test_model.py --checkpoint /home/marisa/models/cleanTübaSmall27  --corpus eval --prefix 27aug_TuSm
+python3 test_model.py --checkpoint /home/marisa/models/27cleanTübaSmall  --corpus eval --prefix 27aug_TuSm
 
 echo "Done with evaluation!"
