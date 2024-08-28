f306f14092dec515ec758dc99e8e5806e2c2674d
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-08-28 17:24:38 +0200

    CCE-01: using correct col names for cleaning

--- src/clean_data.py
+++ src/clean_data.py
@@ -15,40 +15,44 @@ def clean_sentence(sentence):
     print(sentence)
     return sentence
 
-def process_jsonl(input_file, output_file):
+def process_jsonl(input_file, output_file, col):
     with open(input_file, 'r', encoding='utf-8') as infile, \
          open(output_file, 'w', encoding='utf-8') as outfile:
         
         for line in infile:
             data = json.loads(line)
-            if 'text' in data:
-                data['text'] = clean_sentence(data['text'])
+            if col in data:
+                data[col] = clean_sentence(data[col])
             outfile.write(json.dumps(data) + '\n')
 
 print("Cleaning GC4 Data: ")
 input_file = '/home/marisa/data/de_de_pairs.jsonl'
 output_file = '/home/marisa/data/CLEANED_de_de_pairs.jsonl'
-process_jsonl(input_file, output_file)
-
+process_jsonl(input_file, output_file, "text")
+process_jsonl(input_file, output_file, "gold_sentence")
 print("Cleaning TIGER Data: ")
 print("Train: ")
 input_file = '/home/marisa/data/tiger_train.jsonl'
 output_file = '/home/marisa/data/CLEANED_tiger_train.jsonl'
-process_jsonl(input_file, output_file)
+process_jsonl(input_file, output_file, "Original sentence")
+process_jsonl(input_file, output_file, "Canonical form")
 print("Test: ")
 input_file = '/home/marisa/data/tiger_test.jsonl'
 output_file = '/home/marisa/data/CLEANED_tiger_test.jsonl'
-process_jsonl(input_file, output_file)
+process_jsonl(input_file, output_file, "Original sentence")
+process_jsonl(input_file, output_file, "Canonical form")
 
 print("Cleaning TüBa Data: ")
 print("Train: ")
 input_file = '/home/marisa/data/tüba_train.jsonl'
 output_file = '/home/marisa/data/CLEANED_tüba_train.jsonl'
-process_jsonl(input_file, output_file)
+process_jsonl(input_file, output_file, "Treebank-Sentence")
+process_jsonl(input_file, output_file, "Reconstructed-Sentence")
 print("Test: ")
 input_file = '/home/marisa/data/tüba_test.jsonl'
 output_file = '/home/marisa/data/CLEANED_tüba_test.jsonl'
-process_jsonl(input_file, output_file)
+process_jsonl(input_file, output_file, "Treebank-Sentence")
+process_jsonl(input_file, output_file, "Reconstructed-Sentence")
 
 print("Cleaning Eval Data: ")
 input_file = '/home/marisa/data/evaluation_sentences.jsonl'
