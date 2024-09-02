29a1c251efe86d72997fda9cca751292c864f34d
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-09-02 18:26:33 +0200

    CCE-02: write to final goldcol name and add only if not empty

--- src/clean_data.py
+++ src/clean_data.py
@@ -32,36 +32,35 @@ def process_jsonl(input_file, output_file, col):
             data[col] = clean_sentence(data[col])
             outfile.write(json.dumps(data) + '\n')
 
-def add_other_golds(input_file, output_file, sentcol, goldcol):
+def add_other_golds(input_file, output_file, sentcol, goldcol, finalgoldcol):
     with open(input_file, 'r', encoding='utf-8') as infile, \
          open(output_file, 'a', encoding='utf-8') as outfile:
         for line in infile:
             data = json.loads(line)
-            if data[goldcol]:
-                print(data[sentcol] + "\n" + data[goldcol] + "\n")
-                json.dump({sentcol: data[sentcol], goldcol: data[goldcol]}, outfile)
+            if data[goldcol] != " ":
+                json.dump({sentcol: data[sentcol], finalgoldcol: data[goldcol]}, outfile)
                 outfile.write("\n")
 
 print("Getting other gold standards!")
 print("Tiger Train")
 input_file = '/home/marisa/data/tiger_train.jsonl'
 output_file = '/home/marisa/data/ALL_GOLDS_tiger_train.jsonl'
-add_other_golds(input_file, output_file, "Original sentence", "gold2 (LCO)")
+add_other_golds(input_file, output_file, "Original sentence", "gold2 (LCO)", "Canonical form")
 print("Tiger Test")
 input_file = '/home/marisa/data/tiger_test.jsonl'
 output_file = '/home/marisa/data/ALL_GOLDS_tiger_test.jsonl'
-add_other_golds(input_file, output_file, "Original sentence", "gold2 (LCO)")
+add_other_golds(input_file, output_file, "Original sentence", "gold2 (LCO)", "Canonical form")
 
 print("TüBa Train")
 input_file = '/home/marisa/data/tüba_train.jsonl'
 output_file = '/home/marisa/data/ALL_GOLDS_tüba_train.jsonl'
-add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_1")
-add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_2")
+add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_1", "Reconstructed-Sentence")
+add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_2", "Reconstructed-Sentence")
 print("TüBa Test")
 input_file = '/home/marisa/data/tüba_test.jsonl'
 output_file = '/home/marisa/data/ALL_GOLDS_tüba_test.jsonl'
-add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_1")
-add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_2")
+add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_1", "Reconstructed-Sentence")
+add_other_golds(input_file, output_file, "Treebank-Sentence", "Gold_2", "Reconstructed-Sentence")
 
 print("Cleaning GC4 Data: ")
 input_file = '/home/marisa/data/de_de_pairs.jsonl'
