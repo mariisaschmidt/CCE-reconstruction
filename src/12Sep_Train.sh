56ab5a14afb65d859139113deb98a36028797510
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-09-12 08:56:57 +0200

    CCE-02: train merged: old, one gold

--- src/12Sep_Train.sh
+++ src/12Sep_Train.sh
@@ -1,10 +1,14 @@
-echo "Start training of TigerSmall"
+# echo "Start training of TigerSmall"
 
-python3 finetune.py --dataset tiger --model_name  12sep_OneOld_TigerSmall  --pretrained_model /home/marisa/models/Aug25Small
+# python3 finetune.py --dataset tiger --model_name  12sep_OneOld_TigerSmall  --pretrained_model /home/marisa/models/Aug25Small
 
-echo "Start training of TübaSmall"
+# echo "Start training of TübaSmall"
 
-python3 finetune.py --dataset tüba --model_name  12sep_OneOld_TübaSmall  --pretrained_model /home/marisa/models/Aug25Small
+# python3 finetune.py --dataset tüba --model_name  12sep_OneOld_TübaSmall  --pretrained_model /home/marisa/models/Aug25Small
+
+echo "Start training of MergedSmall"
+
+python3 finetune.py --dataset merged --model_name  12sep_OneOld_MergedSmall  --pretrained_model /home/marisa/models/Aug25Small
 
 # echo "Start training of TigerBase"
 
@@ -13,6 +17,10 @@ python3 finetune.py --dataset tüba --model_name  12sep_OneOld_TübaSmall  --pre
 # echo "Start training of TübaBase"
 
 # python3 finetune.py --dataset tüba --model_name  12sep_OneOld_TübaBase  --pretrained_model /home/marisa/models/Aug25Base
+
+# echo "Start training of MergedBase"
+
+# python3 finetune.py --dataset merged --model_name  12sep_OneOld_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Done with training!"
 echo "Running evaluation script:"
--- src/12sep_eval.sh
+++ src/12sep_eval.sh
@@ -1,10 +1,22 @@
 echo "Starting evaluation!"
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus eval --prefix 12sep_OneOld_TiSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus eval --prefix 12sep_OneOld_TiSm
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus tuba --prefix 12sep_OneOld_TiSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus tuba --prefix 12sep_OneOld_TiSm
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus tiger --prefix 12sep_OneOld_TiSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  --corpus tiger --prefix 12sep_OneOld_TiSm
+
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus tuba --prefix 12sep_OneOld_TuSm
+
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus tiger --prefix 12sep_OneOld_TuSm
+
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus eval --prefix 12sep_OneOld_TuSm
+
+python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedSmall  --corpus eval --prefix 12sep_OneOld_MeSm
+
+python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedSmall  --corpus tuba --prefix 12sep_OneOld_MeSm
+
+python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedSmall  --corpus tiger --prefix 12sep_OneOld_MeSm
 
 # python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaBase  --corpus eval --prefix 12sep_OneOld_TuBas
 
@@ -18,10 +30,10 @@ python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerSmall  
 
 # python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TigerBase  --corpus eval --prefix 12sep_OneOld_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus tuba --prefix 12sep_OneOld_TuSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedBase  --corpus eval --prefix 12sep_OneOld_MeBas
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus tiger --prefix 12sep_OneOld_TuSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedBase  --corpus tuba --prefix 12sep_OneOld_MeBas
 
-python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_TübaSmall  --corpus eval --prefix 12sep_OneOld_TuSm
+# python3 test_model.py --checkpoint /home/marisa/models/12sep_OneOld_MergedBase  --corpus tiger --prefix 12sep_OneOld_MeBas
 
 echo "Done with evaluation!"
