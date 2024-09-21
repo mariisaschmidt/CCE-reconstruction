b5c0276b8455b01aa90b93942cffe409c4b33888
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-09-21 07:58:24 +0200

    CCE-08: data now OneOld, prepared scripts for training

--- src/test_model.py
+++ src/test_model.py
@@ -77,17 +77,17 @@ if __name__ == '__main__':
         checkpoint = " "
     
     if args.corpus == "tuba":
-        corpus = os.path.expanduser("~/data/CLEANED_tüba_test.jsonl")
+        corpus = os.path.expanduser("~/data/CLEANED_OLD_tüba_test.jsonl")
         sent_col = "Treebank-Sentence"
         gold_col = "Reconstructed-Sentence"
         add_space = False
     elif args.corpus == "tiger":
-        corpus = os.path.expanduser("~/data/CLEANED_tiger_test.jsonl")
+        corpus = os.path.expanduser("~/data/CLEANED_OLD_tiger_test.jsonl")
         sent_col = "Original sentence"
         gold_col = "Canonical form"
         add_space = True
     elif args.corpus == "eval":
-        corpus = os.path.expanduser("~/data/CLEANED_evaluation_sentences.jsonl")
+        corpus = os.path.expanduser("~/data/CLEANED_OLD_evaluation_sentences.jsonl")
         sent_col = "Sentence"
         gold_col = "Gold"
         add_space = False
