ba0de488ad371da2151f6234037a8195a3e7d8fc
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-09-23 12:35:49 +0200

    CCE-08: redo last eval due to broken pipe

--- src/21sep_eval.sh
+++ src/21sep_eval.sh
@@ -12,27 +12,27 @@ echo "Starting evaluation!"
 
 # python3 test_model.py --checkpoint /home/marisa/models/12sep_AllNew_TübaSmall  --corpus eval --prefix 12sep_AllNew_TuSm
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus eval --prefix 22sep_AllNew_MeSm
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus eval --prefix 22sep_AllNew_MeSm
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus tuba --prefix 22sep_AllNew_MeSm
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus tuba --prefix 22sep_AllNew_MeSm
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus tiger --prefix 22sep_AllNew_MeSm
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedSmall  --corpus tiger --prefix 22sep_AllNew_MeSm
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus eval --prefix 22sep_AllNew_TuBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus eval --prefix 22sep_AllNew_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus tuba --prefix 22sep_AllNew_TuBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus tuba --prefix 22sep_AllNew_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus tiger --prefix 22sep_AllNew_TuBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TübaBase  --corpus tiger --prefix 22sep_AllNew_TuBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus tuba --prefix 22sep_AllNew_TiBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus tuba --prefix 22sep_AllNew_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus tiger --prefix 22sep_AllNew_TiBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus tiger --prefix 22sep_AllNew_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus eval --prefix 22sep_AllNew_TiBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_TigerBase  --corpus eval --prefix 22sep_AllNew_TiBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedBase  --corpus eval --prefix 22sep_AllNew_MeBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedBase  --corpus eval --prefix 22sep_AllNew_MeBas
 
-python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedBase  --corpus tuba --prefix 22sep_AllNew_MeBas
+# python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedBase  --corpus tuba --prefix 22sep_AllNew_MeBas
 
 python3 test_model.py --checkpoint /home/marisa/models/22sep_AllNew_MergedBase  --corpus tiger --prefix 22sep_AllNew_MeBas
 
