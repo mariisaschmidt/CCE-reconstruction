4ff225dcb07c2a2a876e41ee858a8e7fe70b7d42
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-09-27 07:56:45 +0200

    CE-05: fixed learning rate

--- src/train_optimized.py
+++ src/train_optimized.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
             batchsize = 4
             prefix = "reconstruct the ellipsis in this sentence: "
             epochs = 10
-            learning_rate = 7.505076e05
+            learning_rate = 7.505076e-5
         if dataset_name == "g4":
             data = os.path.expanduser("~/data/CLEANED_de_de_pairs.jsonl")
             dataset = load_dataset("json", data_files=data, split='train')
