ecdf66296240ad1af656056b55a695e8cc4c1818
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-10-01 16:54:21 +0200

    CCE-09: filtering works now as expected, retraining now

--- src/01Oct_Train.sh
+++ src/01Oct_Train.sh
@@ -1,29 +1,29 @@
 echo "Start training of TigerSmall"
 
-python3 finetune.py --dataset tiger --model_name  Test_NoNoCCE_OneOld_TigerSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
+python3 finetune.py --dataset tiger --model_name  01oct_NoNoCCE_OneOld_TigerSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
 
-# echo "Start training of TübaSmall"
+echo "Start training of TübaSmall"
 
-# python3 finetune.py --dataset tüba --model_name  01oct_NoNoCCE_OneOld_TübaSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
+python3 finetune.py --dataset tüba --model_name  01oct_NoNoCCE_OneOld_TübaSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
 
-# echo "Start training of MergedSmall"
+echo "Start training of MergedSmall"
 
-# python3 finetune.py --dataset merged --model_name  01oct_NoNoCCE_OneOld_MergedSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
+python3 finetune.py --dataset merged --model_name  01oct_NoNoCCE_OneOld_MergedSmall  --pretrained_model /home/marisa/models/Aug25Small --remove_no_cce 1
 
-# echo "Start training of TigerBase"
+echo "Start training of TigerBase"
 
-# python3 finetune.py --dataset tiger --model_name  01oct_NoNoCCE_OneOld_TigerBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
+python3 finetune.py --dataset tiger --model_name  01oct_NoNoCCE_OneOld_TigerBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
 
-# echo "Start training of TübaBase"
+echo "Start training of TübaBase"
 
-# python3 finetune.py --dataset tüba --model_name  01oct_NoNoCCE_OneOld_TübaBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
+python3 finetune.py --dataset tüba --model_name  01oct_NoNoCCE_OneOld_TübaBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
 
-# echo "Start training of MergedBase"
+echo "Start training of MergedBase"
 
-# python3 finetune.py --dataset merged --model_name  01oct_NoNoCCE_OneOld_MergedBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
+python3 finetune.py --dataset merged --model_name  01oct_NoNoCCE_OneOld_MergedBase  --pretrained_model /home/marisa/models/Aug25Base --remove_no_cce 1
 
-# echo "Done with training!"
-# echo "Running evaluation script:"
+echo "Done with training!"
+echo "Running evaluation script:"
 
-# chmod +x 01oct_eval.sh
-# ./01oct_eval.sh 
+chmod +x 01oct_eval.sh
+./01oct_eval.sh 
