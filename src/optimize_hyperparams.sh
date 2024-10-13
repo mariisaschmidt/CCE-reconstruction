e8712c04f83f3090fb8fdc7c360189ff94bcbdf7
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-10-13 16:37:53 +0200

    CCE-05: updated script

--- src/optimize_hyperparams.sh
+++ src/optimize_hyperparams.sh
@@ -1,5 +1,5 @@
 echo "Start optimization of hyperparams"
 
-python3 finetune.py --dataset merged --model_name  12Oct_Optimized_OneNew_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
+python3 finetune.py --dataset merged --model_name  13Oct_Optimized_OneNew_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Done :)"
