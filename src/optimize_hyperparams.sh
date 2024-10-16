fa639f86492b043226a7978be1652ac2acfdfb21
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-10-16 15:20:22 +0200

    CCE-05: added eval of optimized model

--- src/optimize_hyperparams.sh
+++ src/optimize_hyperparams.sh
@@ -1,5 +1,13 @@
-echo "Start optimization of hyperparams"
+# echo "Start optimization of hyperparams"
 
-python3 finetune.py --dataset merged --model_name  14Okt_Optimized_OneNew_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
+# python3 finetune.py --dataset merged --model_name  14Okt_Optimized_OneNew_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
+
+echo "Evaluating..."
+
+python3 test_model.py --checkpoint /home/marisa/models/14Okt_Optimized_OneNew_MergedBase --corpus tiger --prefix 14Okt_Opt_
+
+python3 test_model.py --checkpoint /home/marisa/models/14Okt_Optimized_OneNew_MergedBase --corpus tuba --prefix 14Okt_Opt_
+
+python3 test_model.py --checkpoint /home/marisa/models/14Okt_Optimized_OneNew_MergedBase --corpus eval --prefix 14Okt_Opt_
 
 echo "Done :)"
