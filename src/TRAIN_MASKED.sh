2e894045d9ed891c4933c832e9ae22f910c44bac
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-11-30 15:13:09 +0100

    CCE-07: added bash script

---
+++ src/TRAIN_MASKED.sh
@@ -0,0 +1,7 @@
+python3 finetuneMasking.py --model_name  3011_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
+
+python3 testMasking.py --checkpoint /home/marisa/models/3011MaskedSmall --prefix 3011_maskedSmall
+
+python3 finetuneMasking.py --model_name  3011_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
+
+python3 testMasking.py --checkpoint /home/marisa/models/3011MaskedBase --prefix 3011_maskedBase
