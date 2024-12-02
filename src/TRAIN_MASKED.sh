68290f3661c7a68fbb7c1f42b7062bbc1883b430
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-12-02 13:47:47 +0100

    CCE-07: got the big gpu now train again

--- src/TRAIN_MASKED.sh
+++ src/TRAIN_MASKED.sh
@@ -1,7 +1,7 @@
-python3 finetuneMasking.py --model_name  3011_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
+python3 finetuneMasking.py --model_name  0212_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
 
-python3 testMasking.py --checkpoint /home/marisa/models/3011MaskedSmall --prefix 3011_maskedSmall
+python3 testMasking.py --checkpoint /home/marisa/models/0212MaskedSmall --prefix 0212_maskedSmall
 
-# python3 finetuneMasking.py --model_name  3011_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
+python3 finetuneMasking.py --model_name  0212_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
 
-# python3 testMasking.py --checkpoint /home/marisa/models/3011MaskedBase --prefix 3011_maskedBase
+python3 testMasking.py --checkpoint /home/marisa/models/0212MaskedBase --prefix 0212_maskedBase
