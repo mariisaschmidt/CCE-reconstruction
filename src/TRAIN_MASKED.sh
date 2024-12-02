8eca62d4c7b47be6a8ac33cd4704d3ec9661ac92
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-12-02 15:04:32 +0100

    CCE-07: use basic t5-small instead of my gc4 to see if this makes a difference in memory use

--- src/TRAIN_MASKED.sh
+++ src/TRAIN_MASKED.sh
@@ -1,11 +1,11 @@
 echo "Start training of MaskedSmall"
-python3 finetuneMasking.py --model_name  0212_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
+python3 finetuneMasking.py --model_name  0212_MaskedSmall #--pretrained_model /home/marisa/models/Aug25Small
 
 echo "Start evaluation of MaskedSmall"
 python3 testMasking.py --checkpoint /home/marisa/models/0212MaskedSmall --prefix 0212_maskedSmall
 
 echo "Start training of MaskedBase"
-python3 finetuneMasking.py --model_name  0212_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
+python3 finetuneMasking.py --model_name  0212_MaskedBase #--pretrained_model /home/marisa/models/Aug25Base
 
 echo "Start evaluation of MaskedBase"
 python3 testMasking.py --checkpoint /home/marisa/models/0212MaskedBase --prefix 0212_maskedBase
