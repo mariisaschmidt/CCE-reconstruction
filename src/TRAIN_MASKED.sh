772ff05985e6bb01c4fd5f22b80f25e9a362425a
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-12-03 15:54:33 +0100

    CCE-07: run test eval

--- src/TRAIN_MASKED.sh
+++ src/TRAIN_MASKED.sh
@@ -1,11 +1,11 @@
-echo "Start training of MaskedSmall"
-python3 finetuneMasking.py --model_name  0312_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
+# echo "Start training of MaskedSmall"
+# python3 finetuneMasking.py --model_name  0312_MaskedSmall --pretrained_model /home/marisa/models/Aug25Small
 
 echo "Start evaluation of MaskedSmall"
-python3 testMasking.py --checkpoint /home/marisa/models/0312MaskedSmall --prefix 0312_maskedSmall
+python3 testMasking.py --checkpoint /home/marisa/models/0312_MaskedSmall --prefix 0312_maskedSmall
 
-echo "Start training of MaskedBase"
-python3 finetuneMasking.py --model_name  0312_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
+# echo "Start training of MaskedBase"
+# python3 finetuneMasking.py --model_name  0312_MaskedBase --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Start evaluation of MaskedBase"
-python3 testMasking.py --checkpoint /home/marisa/models/0312MaskedBase --prefix 0312_maskedBase
+python3 testMasking.py --checkpoint /home/marisa/models/0312_MaskedBase --prefix 0312_maskedBase
