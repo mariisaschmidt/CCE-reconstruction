9150f4e79ce0b0bfdb9fbad1614e7d6b07a904ec
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-12-29 14:05:10 +0100

    CCE-05: add fair erweitert merged data to optimization

--- src/optimize_hyperparams.sh
+++ src/optimize_hyperparams.sh
@@ -1,13 +1,13 @@
 echo "Start optimization of hyperparams"
 
-python3 finetune.py --dataset merged --model_name  23Dec_BLEU_Optimized_AllNew_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
+python3 finetune.py --dataset merged --model_name  30Dec_BLEU_Optimized_AllNewFE_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Evaluating..."
 
-python3 test_model.py --checkpoint /home/marisa/models/23Dec_BLEU_Optimized_AllNew_MergedBase --corpus tiger --prefix 23Dec_BLEU_Opt
+python3 test_model.py --checkpoint /home/marisa/models/30Dec_BLEU_Optimized_AllNewFE_MergedBase --corpus tiger --prefix 30Dec_BLEU_Opt
 
-python3 test_model.py --checkpoint /home/marisa/models/23Dec_BLEU_Optimized_AllNew_MergedBase --corpus tuba --prefix 23Dec_BLEU_Opt
+python3 test_model.py --checkpoint /home/marisa/models/30Dec_BLEU_Optimized_AllNewFE_MergedBase --corpus tuba --prefix 30Dec_BLEU_Opt
 
-python3 test_model.py --checkpoint /home/marisa/models/23Dec_BLEU_Optimized_AllNew_MergedBase --corpus eval --prefix 23Dec_BLEU_Opt
+python3 test_model.py --checkpoint /home/marisa/models/30Dec_BLEU_Optimized_AllNewFE_MergedBase --corpus eval --prefix 30Dec_BLEU_Opt
 
 echo "Done :)"
