66d7022a0c3e79a1acd0843f8940adb37daccbe2
Marisa Schmidt <marisa.schmidt@netcologne.de>
2024-12-30 17:27:09 +0100

    CCE-05: optimize EM score

--- src/finetune.py
+++ src/finetune.py
@@ -109,11 +109,11 @@ def compute_metrics(eval_preds):
 
     decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-    result = {"bleu": result["bleu"]}
+    # result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    # result = {"bleu": result["bleu"]}
 
-    # result = metric_em.compute(predictions=decoded_preds, references=decoded_labels, ignore_case=True, ignore_punctuation=True)
-    # result = {"exact_match": result["exact_match"]}
+    result = metric_em.compute(predictions=decoded_preds, references=decoded_labels, ignore_case=True, ignore_punctuation=True)
+    result = {"exact_match": result["exact_match"]}
 
     #prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
     #result["gen_len"] = np.mean(prediction_lens)
--- src/optimize_hyperparams.sh
+++ src/optimize_hyperparams.sh
@@ -1,9 +1,9 @@
-# echo "Start optimization of hyperparams"
+echo "Start optimization of hyperparams"
 
-# python3 finetune.py --dataset merged --model_name  30Dec_BLEU_Optimized_AllNewFE_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
+python3 finetune.py --dataset merged --model_name  31Dec_EM_Optimized_AllNewFE_MergedBase  --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Evaluating..."
 
-python3 test_model.py --checkpoint /home/marisa/models/30Dec_BLEU_Optimized_AllNewFE_MergedBase --corpus merged --prefix 30Dec_BLEU_Opt
+python3 test_model.py --checkpoint /home/marisa/models/31Dec_EM_Optimized_AllNewFE_MergedBase --corpus merged --prefix 31Dec_EM_Opt
 
 echo "Done :)"
