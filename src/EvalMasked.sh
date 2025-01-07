07c0308a8d90605e93bcf9087d405f2d42ff4ac8
Marisa Schmidt <marisa.schmidt@netcologne.de>
2025-01-07 16:31:59 +0100

    CCE-07: fix Model names

--- src/EvalMasked.sh
+++ src/EvalMasked.sh
@@ -1,9 +1,9 @@
 echo "Start evaluation of Masked Small"
-python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_maskedSmall --corpus merged --prefix 0601_NoMaskedEval_MaskedSmall
+python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_FairMaskedSmall --corpus merged --prefix 0601_NoMaskedEval_MaskedSmall
 
-python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_maskedSmall --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedSmall
+python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_FairMaskedSmall --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedSmall
 
 echo "Start evaluation of Masked Base"
-python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_maskedBase --corpus merged --prefix 0601_NoMaskedEval_MaskedBase
+python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_FairMaskedBase --corpus merged --prefix 0601_NoMaskedEval_MaskedBase
 
-python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_maskedBase --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedBase
+python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_FairMaskedBase --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedBase
