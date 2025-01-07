66aa9f0347307ec6be8eee09f752a48b9c9ed6bd
Marisa Schmidt <marisa.schmidt@netcologne.de>
2025-01-07 16:28:20 +0100

    CCE-07: eval masked with no masked data

--- src/EvalMasked.sh
+++ src/EvalMasked.sh
@@ -1,9 +1,9 @@
 echo "Start evaluation of Masked Small"
-python3 test_model.py --checkpoint /home/marisa/models/0412_MaskedSmall --corpus merged --prefix 0512_NoMaskedEval_MaskedSmall
+python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_MaskedSmall --corpus merged --prefix 0601_NoMaskedEval_MaskedSmall
 
-python3 test_model.py --checkpoint /home/marisa/models/0512_5Ep_MaskedSmall --corpus merged --prefix 0512_NoMaskedEval_5Ep_MaskedSmall
+python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_MaskedSmall --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedSmall
 
 echo "Start evaluation of Masked Base"
-python3 test_model.py --checkpoint /home/marisa/models/0412_MaskedBase --corpus merged --prefix 0512_NoMaskedEval_MaskedBase
+python3 test_model.py --checkpoint /home/marisa/models/0601_10Ep_MaskedBase --corpus merged --prefix 0601_NoMaskedEval_MaskedBase
 
-python3 test_model.py --checkpoint /home/marisa/models/0512_5Ep_MaskedBase --corpus merged --prefix 0512_NoMaskedEval_5Ep_MaskedBase
+python3 test_model.py --checkpoint /home/marisa/models/0601_5Ep_MaskedBase --corpus merged --prefix 0601_NoMaskedEval_5Ep_MaskedBase
