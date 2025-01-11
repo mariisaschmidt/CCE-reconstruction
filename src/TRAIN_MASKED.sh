663258385a9f91ead8f4d464a7f2030bfa2bf1cd
Marisa Schmidt <marisa.schmidt@netcologne.de>
2025-01-11 14:17:08 +0100

    CCE-07: same for 5 Epochs

--- src/TRAIN_MASKED.sh
+++ src/TRAIN_MASKED.sh
@@ -1,11 +1,11 @@
 echo "Start training of CCEFairMaskedSmall"
-python3 finetuneMasking.py --model_name  1101_10Ep_CCEFairMaskedSmall --pretrained_model /home/marisa/models/Aug25Small
+python3 finetuneMasking.py --model_name  1101_5Ep_CCEFairMaskedSmall --pretrained_model /home/marisa/models/Aug25Small
 
 echo "Start evaluation of CCEFairMaskedSmall"
-python3 testMasking.py --checkpoint /home/marisa/models/1101_10Ep_CCEFairMaskedSmall --prefix 1101_10Ep_maskedSmall
+python3 testMasking.py --checkpoint /home/marisa/models/1101_5Ep_CCEFairMaskedSmall --prefix 1101_5Ep_maskedSmall
 
 echo "Start training of CCEFairMaskedBase"
-python3 finetuneMasking.py --model_name  1101_10Ep_CCEFairMaskedBase --pretrained_model /home/marisa/models/Aug25Base
+python3 finetuneMasking.py --model_name  1101_5Ep_CCEFairMaskedBase --pretrained_model /home/marisa/models/Aug25Base
 
 echo "Start evaluation of CCEFairMaskedBase"
-python3 testMasking.py --checkpoint /home/marisa/models/1101_10Ep_CCEFairMaskedBase --prefix 1101_10Ep_maskedBase
+python3 testMasking.py --checkpoint /home/marisa/models/1101_5Ep_CCEFairMaskedBase --prefix 1101_5Ep_maskedBase
